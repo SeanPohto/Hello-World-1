efb9e3aa5a0dcd452dad5c9980cb4282d505f572
Sean Pohto <sean.pohto@mrjaffesclass.com>
2023-09-07 11:31:32 -0700

    commit message

--- DataTypes.java
+++ DataTypes.java
@@ -1,7 +1,10 @@
-public class HelloWorld
+public class DataTypes
 {
     public static void main(String[] args)
     {
         System.out.println("suuii!");
+        int i; 
+        float f = 2.5f;
+        boolean flag = true;
     }
 }
