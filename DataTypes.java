a19cd27463383f93566ff7a9031ee4273c57f1bf
Sean Pohto <sean.pohto@mrjaffesclass.com>
2023-09-12 11:20:20 -0700

    Commit message

--- DataTypes.java
+++ DataTypes.java
@@ -13,5 +13,11 @@ public class DataTypes
         System.out.println(i);
         System.out.println(f);
         System.out.print(flag);
+        String string1 = "I am a string";
+        String string2 = "string2";
+        string1.equals(string2);
+        String string3 = "I am also a string";
+        string1.compareTo(string3);
+        boolean stringsEqual = string1.equals(string3);
     }
 }
